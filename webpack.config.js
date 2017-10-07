52d02640a7dc488485fbebaa468ea80836ceacea
tormozz48 <tormozz48@gmail.com>
2017-10-07 14:01:22 +0300

    Добавил конфигурационный файл для webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
